2badfd54fc2ac2fd5ba007e12b1a359406dd8dab
radu <radustanciu841@yahoo.ro>
2021-06-27 13:32:03 +0300

    Add link to Page Conectare

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Logo from "../assets/images/logo.png";
 import "../pages/Home.css";
+import { Link } from "react-router-dom";
 function Romania() {
   return (
     <div className="header-logo">
       <img src={Logo} alt="" />
-      <button className="btn btn-danger">Conectare</button>
+      <Link to='/login'>
+        <button className="btn btn-danger">Conectare</button>
+      </Link>
     </div>
   );
 }
