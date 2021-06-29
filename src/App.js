4280dfa08a919c5f0c2ed0e0c3292c9b2155b148
radu <radustanciu841@yahoo.ro>
2021-06-29 22:13:01 +0300

    Delet page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from "./pages/Login";
 import Page404 from "./pages/Page404";
 import ÎntrebariFrecevente from "./pages/ÎntrebariFrecevente";
 import Asistenta from "./pages/Asistenta";
-import CentruMedia from "./pages/CentruMedia";
+
 
 function App() {
   return (
@@ -16,7 +16,6 @@ function App() {
         <Route path="/login" component={Login} />
         <Route path='/intrebari_frecvente' component={ÎntrebariFrecevente} />
         <Route path='/asistenta' component={Asistenta}/>
-        <Route path='/centruMedia' component={CentruMedia}/>
         <Route path="*" component={Page404} />
       </Switch>
     </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,20 @@ function Footer() {
     <div className="footer container">
       <p>Ai întrebări? Sună la 0800-672-120</p>
       <div className="row row-cols-4">
-        <div className="col-3">
+        <div className="col-4">
           <Link to="/intrebari_frecvente" className="link-footer">
             Întrebări frecvente
           </Link>
         </div>
-        <div className="col-3">
+        <div className="col-4">
           <Link to="/asistenta" className="link-footer">
             Asistență
           </Link>
         </div>
-        <div className="col-3">
+        <div className="col-4">
           <Link to="/login" className="link-footer">
             Cont
           </Link>
-        </div>
-        <div className="col-3">
-          <Link to="/centruMedia" className='link-footer'>Centru media</Link>
         </div>
       </div>
     </div>
